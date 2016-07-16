3d083e1b396bab092128b638ff1161af3d18ae03
Michael Matveev <michael.s.matveev@gmail.com>
2016-07-16 20:52:40 +0300

    SQL scripts generations for MSSQL

---
+++ SeriesEngine.Core/Reliability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SeriesEngine.Core
+{
+    [Flags]
+    public enum Reliability : short
+    {
+        None                = 0,
+        PreliminaryForecast = 1 << 0,
+        ReliableForecast    = 1 << 1,
+        PreliminaryPlan     = 1 << 2,
+        ReliablePlan        = 1 << 3,
+        OperationalData     = 1 << 4,
+        SpecifiedData       = 1 << 5,
+        CommercialFact      = 1 << 6,
+        FinalFact           = 1 << 7
+    }
+}
